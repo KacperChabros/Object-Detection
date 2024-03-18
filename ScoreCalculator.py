6a9dc245a5034a5962bb03a311ce47efc65d534c
KacperChabros <chabroskacper@gmail.com>
2024-03-18 22:11:08 +0100

    add preprocessing pipeline. Start work on calculating mAP

---
+++ ScoreCalculator.py
@@ -0,0 +1,132 @@
+import torch
+from collections import Counter
+from collections import defaultdict
+
+def intersectionOverUnion(bboxPred, gtBbox):
+    '''
+        ### intersectionOverUnion
+        function that calculates intersection over union for two given bounding boxes
+
+        :param bboxPred: [xMid, yMid, width, height] (torch.tensor)
+        :param gtBbox: [xMid, yMid, width, height] (torch.tensor)
+        :return: numerical value of IoU between two boxes. (torch size of 1)
+    '''
+    detBboxX1 = bboxPred[0] - bboxPred[2] / 2
+    detBboxY1 = bboxPred[1] - bboxPred[3] / 2
+    detBboxX2 = bboxPred[0] + bboxPred[2] / 2
+    detBboxY2 = bboxPred[1] + bboxPred[3] / 2
+
+    gtBboxX1 = gtBbox[0] - gtBbox[2] / 2
+    gtBboxY1 = gtBbox[1] - gtBbox[3] / 2
+    gtBboxX2 = gtBbox[0] + gtBbox[2] / 2
+    gtBboxY2 = gtBbox[1] + gtBbox[3] / 2
+
+    x1 = torch.max(detBboxX1, gtBboxX1)
+    y1 = torch.max(detBboxY1, gtBboxY1)
+    x2 = torch.min(detBboxX2, gtBboxX2)
+    y2 = torch.min(detBboxY2, gtBboxY2)
+
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+
+    detBboxArea = abs((detBboxX2 - detBboxX1) * (detBboxY2 - detBboxY1))
+    gtBboxArea = abs((gtBboxX2 - gtBboxX1) * (gtBboxY2 - gtBboxY1))
+
+    return intersection / (detBboxArea + gtBboxArea - intersection)
+
+
+def meanAveragePrecission(predBboxes, gtBboxes, iouThreshold=0.5, numClasses=80):
+    '''
+        ### meanAveragePrecission
+        calculates the mean average precision for given pred and gt bboxes and given threshold
+
+        :param predBboxes: list of lists in the following format: [imageId, classPred, xMid, yMid, width, height, probScore]
+        :param gtBboxes: list of lists in the following format: [imageId, class, xMid, yMid, width, height]
+        :param iouThreshold: IoU threshold to decide whether two boxes represent the same object. By default set to 0.5
+        :param numClasses: number of classes in the dataset. By default set to 80 (Coco dataset)
+        :return: value of mAP for the given set of pred and gt bboxes and given threshold (torch.tensor size of 1)
+    '''
+    averagePrecissionForClass = []
+
+    # to group predictions by classId (same for ground truths)
+    categoryIdToPredBboxes = defaultdict(list)
+    categoryIdToGTBboxes = defaultdict(list)
+
+    for detection in predBboxes:
+        categoryId = detection[1]
+        categoryIdToPredBboxes[categoryId].append(detection)
+
+    for groundTruth in gtBboxes:
+        categoryId = groundTruth[1]
+        categoryIdToGTBboxes[categoryId].append(groundTruth)
+
+    #calculate for each class
+    for cat in range(numClasses):
+        detectionsForClass = categoryIdToPredBboxes[cat]
+        groundTruthsForClass = categoryIdToGTBboxes[cat]
+
+        #the line below will produce a dictionary with Key=ImageId and Value=how many true bounding boxes it has
+        ammountOfBboxes = Counter([gt[0] for gt in groundTruthsForClass])
+        
+        # this will create a tensor full of zeros instead of just a number of bounding boxes
+        # we do this to keep track of the true bboxes that we have covered so far (only the first pred bbox
+        # that covers the gt bbox is TP, others are FP) 0 = not covered, 1 = covered
+        for imageId, noBboxes in ammountOfBboxes.items():
+            ammountOfBboxes[imageId] = torch.zeros(noBboxes)
+
+        detectionsForClass.sort(key=lambda x: x[6], reverse=True) # sorting predictions by the desc confidence
+
+        # to keep track of which detection is TP:   0-it is not, 1-it is TP
+        #(each class has its own TPs with length of the number of predicted bboxes for this class) 
+        # 1 row - number of detection bboxes columns
+        TPs = torch.zeros(len(detectionsForClass))
+        FPs = torch.zeros(len(detectionsForClass))
+
+        amountOfTrueBboxes = len(gtBboxes)  #number of ground truth bboxes across all images
+
+
+        for detectionId, detection in enumerate(detectionsForClass): #here we've got particular bbox for particular class
+            # we take only those gt bboxes which belong to the same img as considered detection bbox
+            gtsForSameImg = [
+                bbox for bbox in groundTruthsForClass if bbox[0] == detection[0]
+            ]
+            # noTrueBboxesInThisImg = len(gtsForSameImg) # number of true bboxes in the considered img
+
+            maxIoU = 0
+            bestGtId = None
+
+            for gtId, gt in enumerate(gtsForSameImg):#compare taken detection with all GTbboxes from the same img
+                iou = intersectionOverUnion(    #calculate IoU for them
+                    torch.tensor(detection[2:6]),
+                    torch.tensor(gt[2:])
+                )
+
+                if (iou > maxIoU): #check if its better than current max
+                    maxIoU = iou
+                    bestGtId = gtId
+
+            #after checking with all gt bboxes for this img check if maxIoU is greater than threshold
+            if (maxIoU > iouThreshold): 
+                #check if the potential matching GT bbox hasn't been assigned to another detection bbox yet
+                if ( ammountOfBboxes[detection[0]][bestGtId] == 0 ): #(bbox number bestGtId in img detection[0])
+                    TPs[detectionId] = 1
+                    ammountOfBboxes[detection[0]][bestGtId] = 1
+                else:
+                    FPs[detectionId] = 1
+            else:
+                FPs[detectionId] = 1
+
+        #calculate cumsum of TPs and FPs
+        TPsCumsum = torch.cumsum(TPs, dim=0)
+        FPsCumsum = torch.cumsum(FPs, dim=0)
+        
+        recalls = TPsCumsum / amountOfTrueBboxes
+        precisions = TPsCumsum / (TPsCumsum + FPsCumsum)
+
+        #we add point (0,1) in order to provide smooth start to the chart
+        recalls = torch.cat((torch.tensor([0]), recalls))
+        precisions = torch.cat((torch.tensor([1]), precisions))
+
+        # calculate area under the precision-recall curve
+        averagePrecissionForClass.append(torch.trapz(precisions, recalls))
+
+    return sum(averagePrecissionForClass) / len(averagePrecissionForClass)
--- commonCocoPreprocessingFunctions.py
+++ commonCocoPreprocessingFunctions.py
@@ -1,4 +1,8 @@
 import json
+import shutil
+import os
+from collections import defaultdict
+from collections import OrderedDict
 
 class CocoImage:
     def __init__(self, id, fileName, height, width):
@@ -86,9 +90,91 @@ def associateCategoryIdWithItsNameAndYoloId(instancesJSON):
         :param instancesJSON: file with annotations parsed as JSON (e.g. result of getAnnotationsAsJSON)
         :return: dictionary: Key=CategoryId    Value= CocoCategory object containing cocoId, categoryName and yoloId
     '''
-    categoryIdToName = {}
+    categoryIdToNameAndYoloId = {}
     for currentYoloId, category in enumerate(instancesJSON["categories"]):
         cocoId = category["id"]
         categoryName = category["name"]
-        categoryIdToName[cocoId] = CocoCategory(cocoId, categoryName, currentYoloId)
-    return categoryIdToName
+        categoryIdToNameAndYoloId[cocoId] = CocoCategory(cocoId, categoryName, currentYoloId)
+    return categoryIdToNameAndYoloId
+
+
+
+def clearDataSetFromNotAnnotatedImgs(sourceDir, destDir, imageIdToPropsAndAnnots, safe=True):
+    '''
+        ### clearDataSetFromNotAnnotatedImgs
+        clears the desired directory from the images that were not annotated. In Coco dataset they are mostly
+        road signs. The function also clears imageIdToPropsAndAnnots from not annotated images
+        :param sourceDir: directory with the images (with trailing slash)
+        :param destDir: directory for where to move not annotated files to (with trailing slash)
+        :param imageIdToPropsAndAnnots: dictionary: Key=ImageId    Value=CocoImage object (cleared of not annotated images)
+        (e.g.) result of associateCategoryIdWithItsNameAndYoloId
+        :param safe: whether to stop executing when the destDir contains files (set to True by default) (if False,
+        some files may be overwritten)
+        :return: dictionary: Key=ImageId    Value=CocoImage object (cleared of not annotated images)
+    '''
+    if(safe):
+        numberOfFilesInDir = len(os.listdir(destDir))
+        if( numberOfFilesInDir != 0 ):
+            msg = f"The destination directory \"{destDir}\" is not empty! If you wish to move not annotated files anyway"
+            msg += " run the function with safe=False. Some files may be overwritten!"
+            # return imageIdToPropsAndAnnots
+            raise Exception(msg)
+    keysToDelete = []
+    for imageId, image in imageIdToPropsAndAnnots.items():
+        fileName = image.fileName
+        numberOfAnnotations = len(image.annotations)
+        if numberOfAnnotations == 0:
+            shutil.move(sourceDir+fileName, destDir+fileName)
+            keysToDelete.append(imageId)
+    
+    
+    for key in keysToDelete:
+        imageIdToPropsAndAnnots.pop(key)
+    return imageIdToPropsAndAnnots
+
+def createAnnotJSONForYolo(categoryIdToNameAndYoloId, imageIdToPropsAndAnnots, pathToJSONoutfile, safe=True):
+    '''
+        ### createAnnotJSONForYolo
+        creates JSON file with annotations corresponding to the YOLO model output. It is done to 
+        help with calculating mAP later. the format is:
+        {"imageId": [ {"yoloCatId": ycId, "bbox": [xMid, yMid, width, height]}, {(...)anotherAnnot(...)} ]}
+
+        :param categoryIdToNameAndYoloId: Key=CategoryId    Value= CocoCategory object containing cocoId, categoryName and yoloId
+        (e.g.) result of associateCategoryIdWithItsNameAndYoloId function
+        :param imageIdToPropsAndAnnots: dictionary: Key=ImageId    Value=CocoImage object (cleared of not annotated images)
+        (e.g.) result of associateImageIdWithItsPropsAndAnnots function
+        :param pathToJSONoutfile: path where to place JSON file
+        :param safe: whether to check if the JSON file exists (when safe=False, the file may be overwritten!)
+    '''
+    if(safe):
+        if(os.path.exists(pathToJSONoutfile)):
+            msg = f"\"{pathToJSONoutfile}\" already exists! If you want to overwrite it, run the function with safe=False"
+            raise Exception(msg)
+
+    sortedImageIdToAnnots = OrderedDict(sorted(imageIdToPropsAndAnnots.items()))
+    imageIdToListOfAnnotsSerializable = defaultdict(list)
+    for imageId,image in sortedImageIdToAnnots.items():
+        imgWidth = image.width
+        imgHeight = image.height
+        for ann in image.annotations:
+            annotDict = {}
+            cocoCatId = ann["category_id"]
+            yoloCatId = categoryIdToNameAndYoloId[cocoCatId].yoloId
+            bbox = ann["bbox"]
+            x = float(bbox[0])
+            y = float(bbox[1])
+            bboxWidth = float(bbox[2])
+            bboxHeight = float(bbox[3])
+            x = round((x + bboxWidth / 2) / imgWidth,6)
+            y = round((y + bboxHeight / 2) / imgHeight,6)
+            bboxWidth = round(bboxWidth / imgWidth,6)
+            bboxHeight = round(bboxHeight / imgHeight,6)
+            yoloBbox = [x, y, bboxWidth, bboxHeight]
+
+            annotDict["yoloCatId"] = yoloCatId
+            annotDict["bbox"] = yoloBbox
+            imageIdToListOfAnnotsSerializable[imageId].append(annotDict)
+
+
+    with open(pathToJSONoutfile, "w") as outfile:
+        json.dump(imageIdToListOfAnnotsSerializable, outfile)
